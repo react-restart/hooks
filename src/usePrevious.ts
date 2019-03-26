aeb56d8392b261072d356bc1513e3322fa794974
Jason Quense <monastic.panic@gmail.com>
2019-03-26 19:37:27 -0400

    fix: remove usePrevious console.log

--- src/usePrevious.ts
+++ src/usePrevious.ts
@@ -1,9 +1,8 @@
 import { useEffect, useRef } from 'react'
 
-export default function usePrevious(value) {
-  const ref = useRef()
+export default function usePrevious<T>(value: T): T | null {
+  const ref = useRef<T | null>(null)
   useEffect(() => {
-    console.log('update!')
     ref.current = value
   })
   return ref.current
