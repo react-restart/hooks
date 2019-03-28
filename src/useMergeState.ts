c14a4e590a5e9d3a92d6e21b4f81d0f64c627dd8
Jason Quense <monastic.panic@gmail.com>
2019-03-28 11:11:42 -0400

    feat: allow return null from useMergeState updater function

--- src/useMergeState.ts
+++ src/useMergeState.ts
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 
-type Updater<TState> = (state: TState) => Partial<TState>
+type Updater<TState> = (state: TState) => Partial<TState> | null
 
 export type MergeStateSetter<TState> = (
   update: Updater<TState> | Partial<TState> | null
 ) => void
 
-export default function useMergeState<TState>(
+export default function useMergeState<TState extends {}>(
   initialState: TState
 ): [TState, MergeStateSetter<TState>] {
   const [state, setState] = useState<TState>(initialState)
@@ -14,7 +14,10 @@ export default function useMergeState<TState>(
   const updater = (update: Updater<TState> | Partial<TState> | null) => {
     if (update === null) return
     if (typeof update === 'function')
-      setState(state => ({ ...state, ...update(state) }))
+      setState(state => {
+        const nextState = update(state)
+        return nextState == null ? state : { ...state, ...nextState }
+      })
 
     setState(state => ({ ...state, ...update }))
   }
