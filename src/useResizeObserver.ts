5fed7b35ce37bf5e40254698503c190107e628ac
Jason Quense <monastic.panic@gmail.com>
2019-04-15 12:54:17 -0400

    feat; add useResizeObserver

---
+++ src/useResizeObserver.ts
@@ -0,0 +1,47 @@
+import { useLayoutEffect, useState } from 'react'
+
+export interface Rect {
+  width: number
+  height: number
+  x?: number
+  y?: number
+}
+
+type Handler = (contentRect: Rect) => void
+
+const targetMap = new WeakMap<Element, Handler>()
+let resizeObserver: ResizeObserver
+
+function getResizeObserver() {
+  // eslint-disable-next-line no-return-assign
+  return (resizeObserver =
+    resizeObserver ||
+    new window.ResizeObserver(entries => {
+      entries.forEach(entry => {
+        const handler = targetMap.get(entry.target)
+        if (handler) handler(entry.contentRect as any)
+      })
+    }))
+}
+
+export default function useResizeObserver<TElement extends Element>(
+  element: TElement
+): Rect | null {
+  const [rect, setRect] = useState<Rect | null>(null)
+
+  useLayoutEffect(() => {
+    getResizeObserver().observe(element)
+
+    setRect(element.getBoundingClientRect())
+
+    targetMap.set(element, rect => {
+      setRect(rect)
+    })
+
+    return () => {
+      targetMap.delete(element)
+    }
+  }, [element])
+
+  return rect
+}
