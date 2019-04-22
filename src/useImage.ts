4dd3f1756c2a6bda1161a76941c521159fce7f85
Jason Quense <monastic.panic@gmail.com>
2019-04-22 14:11:51 -0400

    docs: useImage example

--- src/useImage.ts
+++ src/useImage.ts
@@ -10,6 +10,25 @@ type State = {
  *
  * @param imageOrUrl The `HtmlImageElement` or image url to load
  * @param crossOrigin The `crossorigin` attribute to set
+ *
+ * ```ts
+ * const { image, error } = useImage('/static/kittens.png')
+ * const ref = useRef<HTMLCanvasElement>()
+ *
+ * useEffect(() => {
+ *   const ctx = ref.current.getContext('2d')
+ *
+ *   if (image) {
+ *     ctx.drawImage(image, 0, 0)
+ *   }
+ * }, [ref, image])
+ *
+ * return (
+ *   <>
+ *     {error && "there was a problem loading the image"}
+ *     <canvas ref={ref} />
+ *   </>
+ * ```
  */
 export default function useImage(
   imageOrUrl?: string | HTMLImageElement | null | undefined,
