541b82c1ae94a82d32174feb65aad2d957a1ce28
Jason Quense <monastic.panic@gmail.com>
2019-07-03 17:28:17 -0400

    feat(useAnimationFrame): add the ability to not clear previous callbacks

--- src/useAnimationFrame.ts
+++ src/useAnimationFrame.ts
@@ -2,11 +2,47 @@ import { useRef } from 'react'
 import useWillUnmount from './useWillUnmount'
 import useMounted from './useMounted'
 
+export interface UseAnimationFrameReturn {
+  cancel(): void
+
+  /**
+   * Request for the provided callback to be called on the next animation frame.
+   * Previously registered callbacks will be cancelled
+   */
+  request(callback: FrameRequestCallback): void
+
+  /**
+   * Request for the provided callback to be called on the next animation frame.
+   * Previously registered callbacks can be cancelled by providing `cancelPrevious`
+   */
+  request(cancelPrevious: boolean, callback: FrameRequestCallback): void
+}
 /**
  * Returns a controller object for requesting and cancelling an animation freame that is properly cleaned up
  * once the component unmounts. New requests cancel and replace existing ones.
+ *
+ * ```tsx
+ * const [style, setStyle] = useState({});
+ * const animationFrame = useAnimationFrame();
+ *
+ * const handleMouseMove = (e) => {
+ *   animationFrame.request(() => {
+ *     setStyle({ top: e.clientY, left: e.clientY })
+ *   })
+ * }
+ *
+ * const handleMouseUp = () => {
+ *   animationFrame.cancel()
+ * }
+ *
+ * return (
+ *   <div onMouseUp={handleMouseUp} onMouseMove={handleMouseMove}>
+ *     <Ball style={style} />
+ *   </div>
+ * )
+ * ```
  */
-export default function useAnimationFrame() {
+export default function useAnimationFrame(): UseAnimationFrameReturn {
   const isMounted = useMounted()
   const handle = useRef<number | undefined>()
 
@@ -19,11 +55,17 @@ export default function useAnimationFrame() {
   useWillUnmount(cancel)
 
   return {
-    request(fn: FrameRequestCallback) {
+    request(
+      cancelPrevious: boolean | FrameRequestCallback,
+      fn?: FrameRequestCallback
+    ) {
       if (!isMounted()) return
 
-      cancel()
-      handle.current = requestAnimationFrame(fn)
+      if (cancelPrevious) cancel()
+
+      handle.current = requestAnimationFrame(
+        fn || (cancelPrevious as FrameRequestCallback)
+      )
     },
     cancel,
   }
