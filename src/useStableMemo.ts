ff981fb8e5b39105ad9e5ba150b0d577e1ec8217
Jason Quense <monastic.panic@gmail.com>
2019-07-03 17:31:27 -0400

    feat(useStableMemo): add useStableMemo for a stable...useMemo
    A useMemo with semantic guarantee that the value won't change when deps stay the same

---
+++ src/useStableMemo.ts
@@ -0,0 +1,46 @@
+import { useCommittedRef } from '.'
+import { DependencyList, useMemo, useState, useRef, useEffect } from 'react'
+
+function isEqual(a: DependencyList, b: DependencyList) {
+  if (a.length !== b.length) return false
+
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      return false
+    }
+  }
+  return true
+}
+
+type DepsCache<T> = {
+  deps?: DependencyList
+  result: T
+}
+
+export function useStableMemo<T>(factory: () => T, deps?: DependencyList): T {
+  let isValid: boolean = true
+
+  const valueRef = useRef<DepsCache<T>>()
+  // initial hook call
+  if (!valueRef.current) {
+    valueRef.current = {
+      deps,
+      result: factory(),
+    }
+    // subsequent calls
+  } else {
+    isValid = !!(
+      deps &&
+      valueRef.current.deps &&
+      isEqual(deps, valueRef.current.deps)
+    )
+  }
+
+  const cache = isValid ? valueRef.current : { deps, result: factory() }
+
+  useEffect(() => {
+    valueRef.current = cache
+  })
+
+  return cache.result
+}
