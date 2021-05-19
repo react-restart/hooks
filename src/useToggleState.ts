5f95dd07dde261f73b8ff658f54dcae9b6034321
Jason Quense <monastic.panic@gmail.com>
2021-05-19 13:34:57 -0400

    feat(useToggleState): add useToggleState hook (#48)
    * feat(useToggleState): add useToggleState hook
    
    * Apply suggestions from code review
    
    Co-authored-by: Kyle Tsang <6854874+kyletsang@users.noreply.github.com>
    
    Co-authored-by: Kyle Tsang <6854874+kyletsang@users.noreply.github.com>

---
+++ src/useToggleState.ts
@@ -0,0 +1,31 @@
+import { useReducer, Reducer } from 'react'
+
+/**
+ * Create a state setter pair for a boolean value that can be "switched".
+ * Unlike `useState(false)`, `useToggleState` will automatically flip the state
+ * value when its setter is called with no argument.
+ *
+ * @param initialState The initial boolean value
+ * @returns A tuple of the current state and a setter
+ *
+ * ```jsx
+ * const [show, toggleShow] = useToggleState(false)
+ *
+ * return (
+ *   <>
+ *     <button onClick={() => toggleShow()}>
+ *       Toggle
+ *     <button>
+ *
+ *     {show && <strong>Now you can see me</strong>}
+ *   </>
+ * )
+ *
+ * ```
+ */
+export default function useToggleState(initialState: boolean = false) {
+  return useReducer<Reducer<boolean, boolean | undefined>>(
+    (state: boolean, action?: boolean) => (action == null ? !state : action),
+    initialState,
+  ) as [boolean, (value?: boolean) => void]
+}
