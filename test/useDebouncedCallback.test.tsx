c8d69b23dd22a802f1c918f791478e841a9d0b99
Jason Quense <monastic.panic@gmail.com>
2023-12-06 11:05:20 -0800

    feat: add leading, and maxWait options to debounce hooks (#97)

--- test/useDebouncedCallback.test.tsx
+++ test/useDebouncedCallback.test.tsx
@@ -1,12 +1,17 @@
+/*! tests an impl adapted from https://github.com/xnimorz/use-debounce/blob/master/test/useDebouncedCallback.test.tsx itself adapted from lodash*/
+
 import useDebouncedCallback from '../src/useDebouncedCallback'
 import { renderHook, act } from '@testing-library/react-hooks'
 
 describe('useDebouncedCallback', () => {
-  it('should return a function that debounces input callback', () => {
+  beforeEach(() => {
     jest.useFakeTimers()
-    const spy = jest.fn()
+  })
 
-    const { result } = renderHook(() => useDebouncedCallback(spy, 500))
+  it('should return a function that debounces input callback', () => {
+    const callback = jest.fn()
+
+    const { result } = renderHook(() => useDebouncedCallback(callback, 500))
 
     act(() => {
       result.current(1)
@@ -14,11 +19,192 @@ describe('useDebouncedCallback', () => {
       result.current(3)
     })
 
-    expect(spy).not.toHaveBeenCalled()
+    expect(callback).not.toHaveBeenCalled()
 
     jest.runOnlyPendingTimers()
 
-    expect(spy).toHaveBeenCalledTimes(1)
-    expect(spy).toHaveBeenCalledWith(3)
+    expect(callback).toHaveBeenCalledTimes(1)
+    expect(callback).toHaveBeenCalledWith(3)
+  })
+
+  it('will call leading callback immediately (but only once, as trailing is set to false)', () => {
+    const callback = jest.fn()
+
+    const { result } = renderHook(() =>
+      useDebouncedCallback(callback, {
+        wait: 1000,
+        leading: true,
+        trailing: false,
+      }),
+    )
+
+    act(() => {
+      result.current(1)
+    })
+
+    expect(callback).toHaveBeenCalledTimes(1)
+
+    act(() => {
+      jest.runOnlyPendingTimers()
+    })
+
+    expect(callback).toHaveBeenCalledTimes(1)
+  })
+
+  it('will call leading callback as well as next debounced call', () => {
+    const callback = jest.fn()
+
+    const { result } = renderHook(() =>
+      useDebouncedCallback(callback, {
+        wait: 1000,
+        leading: true,
+      }),
+    )
+
+    act(() => {
+      result.current()
+      result.current()
+      result.current()
+    })
+
+    expect(callback).toHaveBeenCalledTimes(1)
+
+    act(() => {
+      jest.runOnlyPendingTimers()
+    })
+
+    expect(callback).toHaveBeenCalledTimes(2)
+  })
+
+  it('will call three callbacks if no debounced callbacks are pending', () => {
+    const callback = jest.fn()
+
+    const { result } = renderHook(() =>
+      useDebouncedCallback(callback, {
+        wait: 1000,
+        leading: true,
+      }),
+    )
+
+    act(() => {
+      result.current()
+      result.current()
+      result.current()
+
+      setTimeout(() => {
+        result.current()
+      }, 1001)
+    })
+
+    expect(callback).toHaveBeenCalledTimes(1)
+
+    act(() => {
+      jest.advanceTimersByTime(1001)
+    })
+
+    expect(callback).toHaveBeenCalledTimes(3)
+  })
+
+  it('will call a second leading callback if no debounced callbacks are pending with trailing false', () => {
+    const callback = jest.fn()
+
+    const { result } = renderHook(() =>
+      useDebouncedCallback(callback, {
+        wait: 1000,
+        leading: true,
+        trailing: false,
+      }),
+    )
+
+    act(() => {
+      result.current()
+
+      setTimeout(() => {
+        result.current()
+      }, 1001)
+    })
+
+    expect(callback).toHaveBeenCalledTimes(1)
+
+    act(() => {
+      jest.advanceTimersByTime(1001)
+    })
+
+    expect(callback).toHaveBeenCalledTimes(2)
+  })
+
+  it("won't call both on the leading edge and on the trailing edge if leading and trailing are set up to true and function call is only once", () => {
+    const callback = jest.fn()
+
+    const { result } = renderHook(() =>
+      useDebouncedCallback(callback, {
+        wait: 1000,
+        leading: true,
+      }),
+    )
+
+    act(() => {
+      result.current()
+    })
+
+    expect(callback).toHaveBeenCalledTimes(1)
+
+    act(() => {
+      jest.runAllTimers()
+    })
+
+    expect(callback).toHaveBeenCalledTimes(1)
+  })
+
+  it('will call both on the leading edge and on the trailing edge if leading and trailing are set up to true and there are more than 1 function call', () => {
+    const callback = jest.fn()
+
+    const { result } = renderHook(() =>
+      useDebouncedCallback(callback, {
+        wait: 1000,
+        leading: true,
+      }),
+    )
+
+    act(() => {
+      result.current()
+      result.current()
+    })
+
+    expect(callback).toHaveBeenCalledTimes(1)
+
+    act(() => {
+      jest.runAllTimers()
+    })
+
+    expect(callback).toHaveBeenCalledTimes(2)
+  })
+
+  it('will call callback if maxWait time exceed', () => {
+    const callback = jest.fn()
+
+    const { result } = renderHook(() =>
+      useDebouncedCallback(callback, {
+        wait: 500,
+        maxWait: 600,
+      }),
+    )
+
+    expect(callback).toHaveBeenCalledTimes(0)
+
+    act(() => {
+      result.current()
+      jest.advanceTimersByTime(400)
+    })
+
+    expect(callback).toHaveBeenCalledTimes(0)
+
+    act(() => {
+      result.current()
+
+      jest.advanceTimersByTime(400)
+    })
+
+    expect(callback).toHaveBeenCalledTimes(1)
   })
 })
--- test/useDebouncedValue.test.tsx
+++ test/useDebouncedValue.test.tsx
@@ -4,9 +4,11 @@ import useDebouncedValue from '../src/useDebouncedValue'
 import { act, render } from '@testing-library/react'
 
 describe('useDebouncedValue', () => {
-  it('should return a function that debounces input callback', () => {
+  beforeEach(() => {
     jest.useFakeTimers()
+  })
 
+  it('should return a function that debounces input callback', () => {
     let count = 0
     function Wrapper({ value }) {
       const debouncedValue = useDebouncedValue(value, 500)
@@ -36,5 +38,60 @@ describe('useDebouncedValue', () => {
       expect(getByText('5')).toBeTruthy()
       expect(count).toBe(2)
     })
+  })
+
+  it('will update value immediately if leading is set to true', () => {
+    function Wrapper({ text }) {
+      const value = useDebouncedValue(text, { wait: 1000, leading: true })
+      return <div role="test">{value}</div>
+    }
+    const { rerender, getByText } = render(<Wrapper text={'Hello'} />)
+
+    expect(getByText('Hello')).toBeTruthy()
+
+    act(() => {
+      rerender(<Wrapper text={'Hello world'} />)
+    })
+
+    // value should be set immediately by first leading call
+
+    expect(getByText('Hello world')).toBeTruthy()
+
+    act(() => {
+      rerender(<Wrapper text={'Hello again'} />)
+    })
+
+    // timeout shouldn't have been called yet after leading call was executed
+    expect(getByText('Hello world')).toBeTruthy()
+
+    act(() => {
+      jest.runAllTimers()
+    })
+
+    expect(getByText('Hello again')).toBeTruthy()
+  })
+
+  it('should use isEqual function if supplied', () => {
+    const isEqual = jest.fn((_left: string, _right: string): boolean => true)
+
+    function Wrapper({ text }) {
+      const value = useDebouncedValue(text, { wait: 1000, isEqual })
+      return <div role="test">{value}</div>
+    }
+
+    const { rerender, getByRole } = render(<Wrapper text={'Hello'} />)
+
+    expect(isEqual).toHaveBeenCalledTimes(1)
+
+    act(() => {
+      rerender(<Wrapper text="Test" />)
+
+      jest.runAllTimers()
+    })
+
+    expect(isEqual).toHaveBeenCalledTimes(2)
+    expect(isEqual).toHaveBeenCalledWith('Hello', 'Test')
+
+    expect(getByRole('test').textContent).toEqual('Hello')
   })
 })
